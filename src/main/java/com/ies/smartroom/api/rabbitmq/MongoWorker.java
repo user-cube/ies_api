41c1135e2d103265ade1b23c105ce4e378a280e1
Rui Coelho <ruicoelho@ua.pt>
2019-11-26 10:47:06 +0000

    try deploy

--- src/main/java/com/ies/smartroom/api/rabbitmq/MongoWorker.java
+++ src/main/java/com/ies/smartroom/api/rabbitmq/MongoWorker.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MongoWorker {
 
-    @Value("${spring.data.mongodb.host}")
+    @Value("${spring.data.mongodb.uri}")
     private String host;
 
     @Value("${spring.data.mongodb.database}")
