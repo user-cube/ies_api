56a8b101a2e8c94dbaee1a16d8407ae0ef125856
Jean <jeanhcfb@hotmail.com>
2019-11-26 16:04:38 +0000

    Added gopher connection, not working.

--- src/main/java/com/ies/smartroom/api/rabbitmq/Receiver.java
+++ src/main/java/com/ies/smartroom/api/rabbitmq/Receiver.java
@@ -4,27 +4,27 @@ import org.bson.Document;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
-
+    @Autowired
     public MongoWorker worker;
-
-    public Receiver(){
-        this.worker = new MongoWorker();
-    }
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("amqp://rniqsthq:LTcMEkPr-dN63CAETa0x5jDOM2S_Zm1H@gopher.rmq.cloudamqp.com/rniqsthq",5672);
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("amqp://rniqsthq:LTcMEkPr-dN63CAETa0x5jDOM2S_Zm1H@gopher.rmq.cloudamqp.com/rniqsthq");
+        connectionFactory.setUsername("rniqsthq");
+        connectionFactory.setPassword("LTcMEkPr-dN63CAETa0x5jDOM2S_Zm1H");
+        connectionFactory.setVirtualHost("rniqsthq");
+        connectionFactory.setPort(1883);
+        return connectionFactory;
     }
 
-    @RabbitListener(queues = "hello")
-    @RabbitListener(queues = "hello1")
-    @RabbitListener(queues = "hello3")
+    @RabbitListener(queues = "gen_ata")
     public void processOrder(String data) {
         Document doc = Document.parse(data);
         if (doc.containsKey("temp"))
